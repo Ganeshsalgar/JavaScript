1c460d6830ce3a17c8b2ba9f42b181b221b74f45
Ganeshsalgar <154046754+Ganeshsalgar@users.noreply.github.com>
2024-03-08 13:23:42 +0000

    variables and data types

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let a = 'string'
+var b = 12345
+userstate = "maharstra"
+
+console.table([accountId, a , b , userstate]);
+//let is used beacuse of the it is having a scope
+// var is not having scope
+
+//IMP datatype 
+let c = null
+let d;
+// find the data type 
+console.table([typeof(accountId), typeof(a),typeof b, typeof userstate, typeof c, typeof d ])
