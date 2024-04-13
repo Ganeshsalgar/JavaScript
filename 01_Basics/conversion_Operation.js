676a04eb04c6bc2ed7938ad301d5aba2b4022b7f
Ganeshsalgar <154046754+Ganeshsalgar@users.noreply.github.com>
2024-04-13 11:14:31 +0000

    Operators

---
+++ 01_Basics/conversion_Operation.js
@@ -0,0 +1,10 @@
+let SomNumber = 33;
+
+console.log(SomNumber);
+console.log("print the Number type: - ");
+console.log(typeof SomNumber);
+
+let stringNumber = String(SomNumber);
+console.log("print the String type: - ");
+console.log(typeof stringNumber);
+
---
+++ 01_Basics/operations.js
@@ -0,0 +1,37 @@
+let value = 3;
+let NegValue = -value;
+
+console.log(value);     // Print the Positive value as it is
+console.log(NegValue);  //print the negative value change with sign
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);  // using to find square root
+
+
+let str1 = "Hello";
+let str2 = " Ganesh";
+
+let str3 = str1 + str2; // join the two string into the third string
+
+console.log("print the str3 :-" , str3);
+
+
+//*****************************  bahaviour of the Conversion ****************
+
+console.log( "1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);     //behaviour is first input of the Given like  :- String
+console.log(1 + 2 + "2");    //behaviour is first input of the Given Like :- Number
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;  //preincrement
+gameCounter++;  //postinrement
+console.log(gameCounter);
