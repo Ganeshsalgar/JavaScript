bfbf954927d27901235ca096fb5e105d9fb19015
Ganeshsalgar <154046754+Ganeshsalgar@users.noreply.github.com>
2024-04-13 11:13:57 +0000

    consversion

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===   this is check the  type fo the two two inputs
+
+console.log("2" === 2);
